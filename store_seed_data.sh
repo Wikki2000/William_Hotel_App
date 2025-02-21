8e0f7e636cb25e17e268e6304863ae538bd0bd91
wikki2000 <wisdomokposin@gmail.com>
2025-02-21 23:35:45 +0000

    Add: store_seed_data.sh

---
+++ store_seed_data.sh
@@ -0,0 +1,7 @@
+python3 -m seed_data.rooms.rooms;
+python3 -m seed_data.foods.foods;
+python3 -m seed_data.users.add_users;
+python3 -m seed_data.laundry.laundry;
+python3 -m scripts.users.add_admin;
+python3 -m seed_data.vendors.vendors;
+python3 -m seed_data.games.games
