0efd9628b9246b9615e0fd734d4e3548b28890b3
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2025-04-19 16:19:09 +0100

    Delete mysql/setup_mysql_dev_db.sql

--- mysql/setup_mysql_dev_db.sql
+++
@@ -1,8 +0,0 @@
--- Create database if not exists
-CREATE DATABASE IF NOT EXISTS wills_dev_db;
-
--- Create user if not exists
-CREATE USER IF NOT EXISTS "wills_dev"@"localhost" IDENTIFIED BY "445178aA@";
-
--- Grant priviledges
-GRANT ALL ON wills_dev_db.* TO "wills_dev"@"localhost";
